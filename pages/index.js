b1c4b86166a49930e7bdb8ae311da5f41a849599
Joe Alongi <collectedview@gmail.com>
2021-05-08 15:39:20 -0700

    Calculator Styling
    Calculator Styling
    
    Calculator Styling
    
    Calculator Styling
    
    Calculator Styling

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,10 @@ export default function Home() {
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
-              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-                <h1>Calculate Your Hours</h1>
+              <div className="my-6 text-gray-600 font-bold">
+                <h1 className="text-3xl text-center">Calculate Your Hours</h1>
               </div>
-              <div>
+              <div className="max-w-md mx-auto">
                 <Form />
               </div>
             </div>
